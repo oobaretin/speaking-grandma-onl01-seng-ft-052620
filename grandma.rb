c2864fbe2a8631209697adde7e0187c6f5335f18
osagie obaretin <osagie.codes@gmail.com>
2020-06-01 21:25:14 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,17 +12,6 @@ def speak_to_grandma(phrase)
   end
 end
 
-speak_to_grandma("HELLO THERE")
+speak_to_grandma("I LOVE YOU GRANDMA!")
  
  
-# Whatever you say to grandma, she should respond with
-# HUH?! SPEAK UP, SONNY!
-# unless you shout it (type in all capitals).
-
-# If you shout, she can hear you (or at least she thinks so) 
-# and yells back
-
-# NO, NOT SINCE 1938!
-
-# However if you say 'I LOVE YOU GRANDMA!', she should respond with
-# 'I LOVE YOU TOO PUMPKIN!'
