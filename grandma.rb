4cb2b90e79460c0b05f7f024b497426da7efe1c4
osagie obaretin <osagie.codes@gmail.com>
2020-06-01 21:44:04 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,4 @@
-def speak_to_grandma(phrase="I LOVE YOU GRANDMA!")
+def speak_to_grandma(phrase)
   if phrase=="I LOVE YOU GRANDMA!"
       return "I LOVE YOU TOO PUMPKIN! "
     elsif phrase==phrase.downcase
