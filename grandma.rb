0f9782cdb4c42c3849c02594aabb843945d2a234
osagie obaretin <osagie.codes@gmail.com>
2020-06-01 21:49:25 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,9 +2,9 @@ def speak_to_grandma(phrase)
   if phrase=="I LOVE YOU GRANDMA!"
       puts "I LOVE YOU TOO PUMPKIN! "
     elsif phrase==phrase.downcase
-      puts "HUH?!SPEAK UP,SONNY!"
+      return "HUH?!SPEAK UP,SONNY!"
     elsif phrase==phrase.downcase
-      puts "HUH?!SPEAK UP,SONNY!"
+      return "HUH?!SPEAK UP,SONNY!"
     elsif phrase==phrase.upcase
       return "NO, NOT SINCE 1938!"
   else 
